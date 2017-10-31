38bac078a0d28e3200759c706a0105a3cfb5a17e
Woody134 <mine-1234@qq.com>
2017-10-31 10:30:04 +0800

    finish enter/leave & list animation

--- trans.js
+++ trans.js
@@ -89,3 +89,94 @@ new Vue({
         }
     }
 })
+
+new Vue({
+    el: "#demo9",
+    data: {
+        query: "",
+        list: [
+            { msg: 'Bruce Lee' },
+            { msg: 'Jackie Chan' },
+            { msg: 'Chuck Norris' },
+            { msg: 'Jet Li' },
+            { msg: 'Kung Fury' }
+        ]
+    },
+    computed: {
+        computedList: function(){
+            var vm = this
+            return this.list.filter(function (item){
+                return item.msg.toLowerCase().indexOf(vm.query.toLowerCase()) != -1
+            })
+        }
+    },
+    methods: {
+        beforeEnter: function(el){
+            el.style.opacity = 0
+            el.style.height = 0
+        },
+        enter: function (el, done){
+            var delay = el.dataset.index * 150
+            setTimeout(function () {
+                Velocity(
+                    el,
+                    {opacity: 1, height: '1.6em'},
+                    {complete: done}
+                )
+            }, delay)
+        },
+        leave: function(el, done){
+            var delay = el.dataset.index * 150
+            setTimeout(function () {
+                Velocity(
+                    el,
+                    {opacity: 0, height: 0},
+                    {complete: done}
+                )
+            }, delay)
+        }
+    }
+})
+
+new Vue({
+    el: "#demo10",
+    data: {
+        show: true,
+        fadeInDuration: 1000,
+        fadeOutDuration: 1000,
+        maxFadeDuration: 1500,
+        stop:true
+    },
+    mounted: function () {
+        this.show = false
+    },
+    methods: {
+        beforeEnter: function (el) {
+            el.style.opacity = 0
+        },
+        enter: function (el, done) {
+            var vm = this
+            Velocity(el,
+                { opacity: 1 },
+                {
+                    duration: this.fadeInDuration,
+                    complete: function () {
+                        done()
+                        if (!vm.stop) vm.show = false
+                    }
+                })
+        },
+        leave: function (el, done) {
+            var vm = this
+            Velocity(el,
+                { opacity: 0 },
+                { duration: this.fadeOutDuration,
+                  complete: function() {
+                      done()
+                      vm.show = true
+                  }
+                }
+            )
+        }
+    }
+})
